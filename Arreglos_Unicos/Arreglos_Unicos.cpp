cb464975fb8b8c27c788b5c4034611a467c2fc13
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-05-31 11:49:52 -0400

    Intercambiando valores a un arreglo del vector en C++.

--- Arreglos_Unicos/Arreglos_Unicos.cpp
+++ Arreglos_Unicos/Arreglos_Unicos.cpp
@@ -4,10 +4,23 @@ using namespace std;
 
 int main()
 {
+    /*
     int contador = 0; // EMPIEZA EL CONTADOR INICIAL EN 10.
-    string arreglo[5] = { "primero", "segundo", "tercero", "cuarto", "quinto" };
+    string arreglo[2] = { "primero", "segundo" };
 
-    while (contador < 5) // MIENTRAS QUE EL CONTADOR ES MENOR QUE 5...
+    while (contador < 2) // MIENTRAS QUE EL CONTADOR ES MENOR QUE 5...
+    {
+        cout << arreglo[contador] << endl; // IMPRIMIMOS UNA CASILLA DEL ARREGLO.
+        contador++; // INCREMENTAMOS EL VALOR DEL CONTADOR EN 1 EN CADA VUELTA DEL BUCLE.
+    }
+
+    */
+
+    int contador = 0; // EMPIEZA EL CONTADOR INICIAL EN 10.
+
+    string arreglo[3] = { "primero", "segundo", "tercero" };
+
+    while (contador < 3) // MIENTRAS QUE EL CONTADOR ES MENOR QUE 5...
     {
         cout << arreglo[contador] << endl; // IMPRIMIMOS UNA CASILLA DEL ARREGLO.
         contador++; // INCREMENTAMOS EL VALOR DEL CONTADOR EN 1 EN CADA VUELTA DEL BUCLE.
