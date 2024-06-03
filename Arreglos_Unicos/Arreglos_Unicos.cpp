4c70fea758e8e427078265b0f32e62d6c72c3c3a
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-06-03 11:36:08 -0400

    Algoritmo para desplegar valores dentro de un arreglo.

--- Arreglos_Unicos/Arreglos_Unicos.cpp
+++ Arreglos_Unicos/Arreglos_Unicos.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
     /*
+    
     int contador = 0; // EMPIEZA EL CONTADOR INICIAL EN 10.
     string arreglo[2] = { "primero", "segundo" };
 
@@ -16,6 +17,8 @@ int main()
 
     */
 
+    /*
+    
     int contador = 0; // EMPIEZA EL CONTADOR INICIAL EN 10.
 
     string arreglo[3] = { "primero", "segundo", "tercero" };
@@ -24,6 +27,25 @@ int main()
     {
         cout << arreglo[contador] << endl; // IMPRIMIMOS UNA CASILLA DEL ARREGLO.
         contador++; // INCREMENTAMOS EL VALOR DEL CONTADOR EN 1 EN CADA VUELTA DEL BUCLE.
+    }
+
+    */
+
+    int contador = 0;
+    int array[20]; // CREAMOS UN ARREGLO DE TIPO NUMÉRICO VACÍO.
+
+    while (contador < 20)
+    {
+        array[contador] = contador + 1; // SE GUARDA EN LA CASILLA ACTUAL DE UN ARREGLO...
+        contador++; // ...DE ESTA FORMA, SI EL VALOR DEL CONTADOR ACTUAL DE UN ARREGLO ES 0, ENTONCES SE GUARDARÁ EN EL ARREGLO 1!
+    }
+
+    contador = 0; // RESETEAMOS EL VALOR A 0 PORQUE TRAS EL BUCLE ANTERIOR SU VALOR ES 20...
+
+    while (contador < 20)
+    {
+        cout << array[contador] << endl; // IMPRIME POR CADA LÍNEA LOS VALORES DEL ARREGLO.
+        contador++;
     }
 
     system("pause");
